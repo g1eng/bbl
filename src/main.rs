290e62b58976b2deb8e7d8e8c7d1932eaa02550c
Suzume Nomura <suzume315@g00.g1e.org>
2021-07-31 13:46:06 +0900

    added command line control flow

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@ use std::process::exit;
 use std::fs;
 use std::fs::Permissions;
 use std::os::unix::fs::{PermissionsExt};
+use clap::{App,Arg};
 
 fn is_executable(desc :String) -> bool {
     let path = PathBuf::from(desc);
-    let metadata = fs::metadata(path);
-    let permission = Permissions::from(metadata.unwrap().permissions());
-    // println!("permission {:o}", permission.mode());
-    let user_perm = (permission.mode() >> 6) % 8;
-    // println!("user_perm {:o}", user_perm);
-    user_perm == 7 || user_perm == 5
+    if path.is_file() {
+        println!("exist");
+        let metadata = fs::metadata(path);
+        let permission = Permissions::from(metadata.unwrap().permissions());
+        // println!("permission {:o}", permission.mode());
+        let user_perm = (permission.mode() >> 6) % 8;
+        // println!("user_perm {:o}", user_perm);
+        user_perm == 7 || user_perm == 5
+    } else {
+        false
+    }
 }
 
 fn is_file(desc :String) -> bool {
@@ -19,24 +25,29 @@ fn is_file(desc :String) -> bool {
     path.is_file()
 }
 
-fn parse() -> Result<String, &'static str>{
-    match is_file(String::from("ok")) {
-        true => {
-            println!("exist");
-            if is_executable(String::from("ok")) {
-                Ok(String::from("executable"))
-            } else {
-                Err("is not a file")
-            }
-        },
-        false => {
-            Err("no there")
-        }
+fn parse(file :String) -> Result<String, &'static str>{
+    if is_executable(file) {
+        Ok(String::from("executable"))
+    } else {
+        Err("is not a executable")
     }
 }
 
 fn main() {
-    match parse() {
+    let app = App::new("riosh")
+        .version("0.1.0")
+        .author("Nomura Suzume")
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .index(1)
+                .help("executable to run")
+                .required(true)
+        )
+        .get_matches();
+
+
+    match parse(String::from(app.value_of("file").unwrap())) {
         Ok(r) => println!("{:?}",r),
         Err(e) => {
             eprintln!("{:?}",e);
